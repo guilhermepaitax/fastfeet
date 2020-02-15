b0f65d6b71c71b17ec1963feb61b77bd2a53fa13
Guilherme Paitax <guilhermepaitax16@gmail.com>
2020-02-15 16:45:21 -0300

    movendo arquivos backend

--- src/routes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-
-const routes = new Router();
-
-// routes.post('/', controller.store);
-
-export default routes;
--- src/server.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable no-console */
-import app from './app';
-
-app.listen(process.env.APP_PORT, () => {
-  console.log(`Express server running on ${process.env.APP_URL}`);
-});
